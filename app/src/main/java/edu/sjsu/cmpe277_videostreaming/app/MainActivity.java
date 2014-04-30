b04397da2d6756636c2077a98901bc7541b2bd06
Jon Guan <jon@scanadu.com>
2014-04-30 16:58:14 -0700

    sample code for taking video

--- app/src/main/java/edu/sjsu/cmpe277_videostreaming/app/MainActivity.java
+++ app/src/main/java/edu/sjsu/cmpe277_videostreaming/app/MainActivity.java
@@ -1,13 +1,12 @@
 package edu.sjsu.cmpe277_videostreaming.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.util.Log;
-
-import java.io.Console;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -41,10 +40,14 @@ public class MainActivity extends ActionBarActivity {
 
     public void streamVideo(View view) {
         Log.v(LOG_HANDLE, "stream video clicked");
+        Intent intent = new Intent(this, VideoRecordActivity.class );
+        startActivity(intent);
     }
 
     public void watchVideo(View view) {
+
         Log.v(LOG_HANDLE, "watch video clicked");
     }
+
 
 }
